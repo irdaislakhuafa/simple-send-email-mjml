c5585ac2f6ec13ba3099760f70c2f2300c9be098
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-06-07 00:41:57 +0700

    first commit

---
+++ src/main/java/com/irdaislakhuafa/simplesendemailmjml/SimpleSendEmailMjmlApplication.java
@@ -0,0 +1,13 @@
+package com.irdaislakhuafa.simplesendemailmjml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSendEmailMjmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSendEmailMjmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irdaislakhuafa/simplesendemailmjml/SimpleSendEmailMjmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irdaislakhuafa.simplesendemailmjml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSendEmailMjmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
